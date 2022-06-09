37c8803ead0d44ec2e36935eae90c6607fba8fbb
warsilyn04 <canadorawarren@gmail.com>
2022-06-09 20:04:04 +0800

    first git repository

---
+++ database/migrations/2022_06_08_153203_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('room_number');
+            $table->string('room_rate');
+            $table->integer('number_of_beds');
+            $table->integer('time_limit');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
